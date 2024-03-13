3b8b3c1a8a7f5044f74110c8088170cc0f5df14c
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-03-13 12:03:23 +0200

    Finished solving problem 2164.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sergiovaneg/GO_leetcode
+
+go 1.21.8
---
+++ p2164/sort_even_odd.go
@@ -0,0 +1,41 @@
+package p2164
+
+import (
+	"slices"
+)
+
+func SortEvenOdd(nums []int) []int {
+	// defaults to 0
+	var nums_even []int = make([]int, 0, 50)
+	var nums_odd []int = make([]int, 0, 50)
+
+	var current_even bool = true
+	var count_even = (len(nums) + 1) / 2
+	var count_odd = len(nums) / 2
+
+	for _, element := range nums {
+		if current_even {
+			nums_even = append(nums_even, element)
+		} else {
+			nums_odd = append(nums_odd, element)
+		}
+		current_even = !current_even
+	}
+
+	slices.Sort(nums_even)
+	slices.Sort(nums_odd)
+
+	j := 0
+	for idx := 0; idx < count_even; idx++ {
+		nums[j] = nums_even[idx]
+		j += 2
+	}
+
+	j = 1
+	for idx := count_odd - 1; idx >= 0; idx-- {
+		nums[j] = nums_odd[idx]
+		j += 2
+	}
+
+	return nums
+}
