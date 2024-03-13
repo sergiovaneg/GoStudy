b5d222019cc8f8e60932cfdc0bc415820b009bae
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-03-13 18:26:23 +0200

    Finished solving problem 2485 (pivot integer).

---
+++ p2485/pivot_integer.go
@@ -0,0 +1,35 @@
+package p2485
+
+func PivotInteger(n int) int {
+	if n == 1 {
+		return 1
+	} else if n == 2 {
+		return -1
+	}
+
+	n_halves := n / 2
+	n_minus_n_halves := n - n_halves
+
+	cumsum := make([]int, n_minus_n_halves)
+	cumsum[0] = 1
+
+	var idx int
+
+	for idx = 2; idx < n_halves+2; idx++ {
+		cumsum[0] += idx
+	}
+
+	for idx = 1; idx < n_minus_n_halves; idx++ {
+		cumsum[idx] = cumsum[idx-1] + (idx + n_halves + 1)
+	}
+
+	idx = n_minus_n_halves - 2
+	thr := cumsum[n_minus_n_halves-1] + n_halves + 1
+	for (cumsum[idx] + cumsum[idx] - idx) > thr {
+		idx--
+	}
+	if (cumsum[idx] + cumsum[idx] - idx) == thr {
+		return idx + n_halves + 1
+	}
+	return -1
+}
