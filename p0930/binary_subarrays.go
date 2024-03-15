a950334d938606d2c202e1c15fba5364180bd41e
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-03-15 08:24:49 +0100

    Finished solving problem 930 (Binary Subarrays with Sum) with at-most approach.

--- p0930/binary_subarrays.go
+++ p0930/binary_subarrays.go
@@ -1,23 +1,21 @@
 package p0930
 
 func NumSubarraysWithSum(nums []int, goal int) int {
-	acc := 0
-	cnt := 0
-	for idx_0 := 0; idx_0 < len(nums); idx_0++ {
-		acc = nums[idx_0]
-		if acc == goal {
-			cnt++
-		} else if acc > goal {
-			continue
+	return atMost(nums, goal) - atMost(nums, goal-1)
+}
+
+func atMost(nums []int, goal int) int {
+	var idx_0, idx_1 = 0, 0
+	acc, count := 0, 0
+
+	for idx_1 = 0; idx_1 < len(nums); idx_1++ {
+		acc += nums[idx_1]
+		for (acc > goal) && (idx_0 <= idx_1) {
+			acc -= nums[idx_0]
+			idx_0++
 		}
-		for idx_1 := idx_0 + 1; idx_1 < len(nums); idx_1++ {
-			acc += nums[idx_1]
-			if acc == goal {
-				cnt++
-			} else if acc > goal {
-				break
-			}
-		}
+		count += idx_1 - idx_0 + 1
 	}
-	return cnt
+
+	return count
 }
