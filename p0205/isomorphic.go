2f4a954857185bcc1ec75f800dd92edd818e9cd4
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-04-02 10:00:49 +0300

    Solved problem 205 (Isomorphic Strings) using a double-array approach.

--- p0205/isomorphic.go
+++ p0205/isomorphic.go
@@ -1,34 +1,15 @@
 package p0205
 
 func IsIsomorphic(s string, t string) bool {
-	dict_fwd := make(map[byte]struct {
-		recorded bool
-		val      byte
-	})
-	dict_bwd := make(map[byte]struct {
-		recorded bool
-		val      byte
-	})
+	s2t := [128]int{}
+	t2s := [128]int{}
 
-	for idx := 0; idx < len(s); idx++ {
-		if !dict_fwd[s[idx]].recorded {
-			dict_fwd[s[idx]] = struct {
-				recorded bool
-				val      byte
-			}{true, t[idx]}
-		} else if dict_fwd[s[idx]].val != t[idx] {
+	for idx, end := 0, len(s); idx < end; idx++ {
+		if s2t[s[idx]] != t2s[t[idx]] {
 			return false
-		}
-
-		if !dict_bwd[t[idx]].recorded {
-			dict_bwd[t[idx]] = struct {
-				recorded bool
-				val      byte
-			}{true, s[idx]}
-		} else if dict_bwd[t[idx]].val != s[idx] {
-			return false
+		} else {
+			s2t[s[idx]], t2s[t[idx]] = idx+1, idx+1
 		}
 	}
-
 	return true
 }
