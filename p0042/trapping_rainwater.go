41dd2eb1d5f652ce35adee8dc5b176fac11c84c0
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-04-12 09:48:10 +0300

    Improved code organization for problem 42 (Trapping Rain Water).

--- p0042/trapping_rainwater.go
+++ p0042/trapping_rainwater.go
@@ -2,6 +2,25 @@ package p0042
 
 import "slices"
 
+// Complexity: O(N log N + N) << O(N log N)
+func getUniqueDescending(height []int) []int {
+	sorted_height := make([]int, len(height))
+	unique_height := make([]int, len(height))
+	unique_idx := 0
+
+	copy(sorted_height, height)
+	slices.SortStableFunc(sorted_height, func(a, b int) int { return b - a })
+	unique_height[0] = sorted_height[0]
+	for _, h := range sorted_height[1:] {
+		if h < unique_height[unique_idx] {
+			unique_idx++
+			unique_height[unique_idx] = h
+		}
+	}
+
+	return slices.Clip(unique_height[:unique_idx+1])
+}
+
 func Trap(height []int) int {
 	// Definitions
 	// N: len(height)
@@ -10,28 +29,17 @@ func Trap(height []int) int {
 
 	// Memory: O(N)
 	height = append([]int{slices.Min(height)}, height...)
+	unique_height := getUniqueDescending(height[1:])
 	leveled_height := make([]int, len(height))
 	diff_height := make([]int, len(height)-1)
-	sorted_height := make([]int, len(height)-1)
-	unique_height := make([]int, 0, len(height)-1)
-
-	// Complexity: O(N log N + N) << O(N log N)
-	copy(sorted_height, height[1:])
-	slices.Sort(sorted_height)
-	unique_height = append(unique_height, sorted_height[0])
-	for _, h := range sorted_height[1:] {
-		if h != unique_height[len(unique_height)-1] {
-			unique_height = append(unique_height, h)
-		}
-	}
 
 	// Complexity: O(k*N)
-	for h_idx := len(unique_height) - 1; h_idx > 0; h_idx-- {
-		for idx := 1; idx < len(height); idx++ {
-			if height[idx] == unique_height[h_idx] {
-				leveled_height[idx] = 1
+	for h_idx, uh := range unique_height[:len(unique_height)-1] {
+		for idx, h := range height[1:] {
+			if h == uh {
+				leveled_height[idx+1] = 1
 			}
-			diff_height[idx-1] = leveled_height[idx] - leveled_height[idx-1]
+			diff_height[idx] = leveled_height[idx+1] - leveled_height[idx]
 		}
 
 		idx_0 := 0
@@ -39,7 +47,7 @@ func Trap(height []int) int {
 			if diff_height[idx_0] == -1 {
 				for idx_1, e_1 := range diff_height[idx_0+1:] {
 					if e_1 == 1 {
-						acc += (idx_1 + 1) * (unique_height[h_idx] - unique_height[h_idx-1])
+						acc += (idx_1 + 1) * (uh - unique_height[h_idx+1])
 						idx_0 += idx_1 + 1
 						break
 					}
