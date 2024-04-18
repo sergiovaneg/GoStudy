29aa2faf2c50bb07a4cda830b63af4fcc08bbfed
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-04-18 14:59:02 +0300

    Added single-threaded recursive and (parallel/single-threaded) iterative solutions for problem 463 (Island Perimeter).

--- p0463/island_perimeter.go
+++ p0463/island_perimeter.go
@@ -69,7 +69,7 @@ func (g *GridChecked) exploreCell(row, col int, wg *sync.WaitGroup) {
 	g.mu_perimeter.Unlock()
 }
 
-func IslandPerimeter(grid [][]int) int {
+func IslandPerimeterParallelRecursive(grid [][]int) int {
 	rows, cols := len(grid), len(grid[0])
 	checked := GridChecked{
 		rows:      rows,
@@ -104,4 +104,157 @@ func IslandPerimeter(grid [][]int) int {
 	wg.Wait()
 
 	return checked.perimeter
+}
+
+func IslandPerimeterRecursive(grid [][]int) int {
+	rows, cols := len(grid), len(grid[0])
+	mark_grid := make([][]bool, rows)
+	var perimeter int
+
+	for row := 0; row < rows; row++ {
+		mark_grid[row] = make([]bool, cols)
+	}
+
+	var row, col int
+	for row = 0; row < rows; row++ {
+		found := false
+		for col = 0; col < cols; col++ {
+			if grid[row][col] == 1 {
+				found = true
+				mark_grid[row][col] = true
+				break
+			}
+		}
+		if found {
+			break
+		}
+	}
+
+	var mark_and_spawn func(int, int)
+	var explore_cell func(int, int)
+
+	mark_and_spawn = func(row, col int) {
+		// Selected is water
+		if grid[row][col] == 0 {
+			perimeter++
+			return
+		}
+
+		if !mark_grid[row][col] { // Not yet inspected
+			mark_grid[row][col] = true // Mark as inspected
+			explore_cell(row, col)
+		}
+	}
+
+	explore_cell = func(row, col int) {
+		// Check top
+		if row > 0 {
+			mark_and_spawn(row-1, col)
+		} else {
+			perimeter++
+		}
+
+		// Check Left
+		if col > 0 {
+			mark_and_spawn(row, col-1)
+		} else {
+			perimeter++
+		}
+
+		// Check Bottom
+		if row < rows-1 {
+			mark_and_spawn(row+1, col)
+		} else {
+			perimeter++
+		}
+
+		// Check Right
+		if col < cols-1 {
+			mark_and_spawn(row, col+1)
+		} else {
+			perimeter++
+		}
+	}
+
+	explore_cell(row, col)
+
+	return perimeter
+}
+
+func IslandPerimeterParallelIterative(grid [][]int) int {
+	var perimeter int
+	rows, cols := len(grid), len(grid[0])
+
+	var wg sync.WaitGroup
+	c := make(chan int, rows)
+
+	wg.Add(rows)
+	for row := 0; row < rows; row++ {
+		go func(row int, c chan<- int, wg *sync.WaitGroup) {
+			defer wg.Done()
+			var perimeter int
+			for col := 0; col < cols; col++ {
+				if grid[row][col] == 0 {
+					continue
+				}
+
+				// Check Top
+				if row == 0 || grid[row-1][col] == 0 {
+					perimeter++
+				}
+				// Check Left
+				if col == 0 || grid[row][col-1] == 0 {
+					perimeter++
+				}
+				// Check Bottom
+				if row == rows-1 || grid[row+1][col] == 0 {
+					perimeter++
+				}
+				// Check Right
+				if col == cols-1 || grid[row][col+1] == 0 {
+					perimeter++
+				}
+			}
+			c <- perimeter
+		}(row, c, &wg)
+	}
+	wg.Wait()
+
+	for row := 0; row < rows; row++ {
+		perimeter += <-c
+	}
+
+	return perimeter
+}
+
+func IslandPerimeterIterative(grid [][]int) int {
+	var perimeter int
+
+	rows, cols := len(grid), len(grid[0])
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			if grid[row][col] == 0 {
+				continue
+			}
+
+			// Check Top
+			if row == 0 || grid[row-1][col] == 0 {
+				perimeter++
+			}
+			// Check Left
+			if col == 0 || grid[row][col-1] == 0 {
+				perimeter++
+			}
+			// Check Bottom
+			if row == rows-1 || grid[row+1][col] == 0 {
+				perimeter++
+			}
+			// Check Right
+			if col == cols-1 || grid[row][col+1] == 0 {
+				perimeter++
+			}
+		}
+	}
+
+	return perimeter
 }
--- p0463/island_perimeter_test.go
+++ p0463/island_perimeter_test.go
@@ -10,20 +10,22 @@ func TestIslandPerimeter(t *testing.T) {
 	var grid [][]int
 	var res int
 
+	IslandPerimeter := p0463.IslandPerimeterParallelIterative
+
 	grid = [][]int{{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}}
-	res = p0463.IslandPerimeter(grid)
+	res = IslandPerimeter(grid)
 	if res != 16 {
 		t.Fatalf("Expected 16; got %v", res)
 	}
 
 	grid = [][]int{{1}}
-	res = p0463.IslandPerimeter(grid)
+	res = IslandPerimeter(grid)
 	if res != 4 {
 		t.Fatalf("Expected 4; got %v", res)
 	}
 
 	grid = [][]int{{1, 0}}
-	res = p0463.IslandPerimeter(grid)
+	res = IslandPerimeter(grid)
 	if res != 4 {
 		t.Fatalf("Expected 4; got %v", res)
 	}
