1783d8d483936f5a4605e549b444e7392a04d7cf
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-04-27 12:25:41 +0200

    Optimized problem 1289 for better early returns, memory allocation, and integer division.

--- p1289/minimum_falling_path.go
+++ p1289/minimum_falling_path.go
@@ -1,51 +1,66 @@
 package p1289
 
+func getCost(row1, row2, cost []int) {
+	for idx1, val1 := range row1 {
+		min_value := 100
+		for idx2, val2 := range row2 {
+			if idx1 == idx2 {
+				continue
+			}
+			if val2 < min_value {
+				min_value = val2
+			}
+		}
+		cost[idx1] = val1 + min_value
+	}
+}
+
+func minAllowed(row []int, forbidden int) [2]int {
+	min_idx, min_val := -1, 300
+	for idx, val := range row {
+		if idx == forbidden {
+			continue
+		}
+		if val < min_val {
+			min_idx = idx
+			min_val = val
+		}
+	}
+	return [2]int{min_idx, min_val}
+}
+
 func MinFallingPathSum(grid [][]int) int {
-	//Early return
-	if len(grid) == 1 {
+	// First early return
+	n := len(grid)
+	if n == 1 {
 		return grid[0][0]
 	}
 
-	get_cost := func(row1, row2 []int) []int {
-		cost := make([]int, len(row1))
-		for idx1, val1 := range row1 {
-			min_value := 100
-			for idx2, val2 := range row2 {
-				if idx1 == idx2 {
-					continue
-				}
-				if val2 < min_value {
-					min_value = val2
-				}
-			}
-			cost[idx1] = val1 + min_value
-		}
-		return cost
+	cost0 := make([]int, n)
+
+	// Second early return
+	if n == 2 {
+		getCost(grid[0], grid[1], cost0)
+		return minAllowed(cost0, -1)[1]
 	}
 
-	min_allowed := func(row []int, forbidden int) [2]int {
-		min_idx, min_val := -1, 300
-		for idx, val := range row {
-			if idx == forbidden {
-				continue
-			}
-			if val < min_val {
-				min_idx = idx
-				min_val = val
-			}
-		}
-		return [2]int{min_idx, min_val}
-	}
-
-	// Init
-	n, res, forbidden := len(grid), 0, -1
+	res, forbidden := 0, -1
+	cost1 := make([]int, n)
 
 	for i := 0; i < n-2; i++ {
-		forbidden = min_allowed(
-			get_cost(grid[i], get_cost(grid[i+1], grid[i+2])),
-			forbidden)[0]
+		// Get the cost of choosing each element on the next row
+		getCost(grid[i+1], grid[i+2], cost1)
+
+		// Use the future cost to calculate the current row's cost
+		getCost(grid[i], cost1, cost0)
+
+		// Find optimal selection and restrict next selection
+		forbidden = minAllowed(cost0, forbidden)[0]
+
+		// Update result
 		res += grid[i][forbidden]
 	}
 
-	return res + min_allowed(get_cost(grid[n-2], grid[n-1]), forbidden)[1]
+	// Skip calculation since already performed
+	return res + minAllowed(cost1, forbidden)[1]
 }
