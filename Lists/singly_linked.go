5619625c1712f7d4dc65ea4000130c4d7ce2da8c
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-05-06 09:58:25 +0200

    Extended Singly-Linked list capabilities by adding a String() method.

--- Lists/singly_linked.go
+++ Lists/singly_linked.go
@@ -1,5 +1,7 @@
 package lists
 
+import "fmt"
+
 type SinglyLinkedNode[T any] struct {
 	Val  T
 	Next *SinglyLinkedNode[T]
@@ -18,18 +20,30 @@ func MakeSinglyLinkedList[T any](elements []T) *SinglyLinkedNode[T] {
 func CompareSinglyLinkedLists[T comparable](
 	list1 *SinglyLinkedNode[T],
 	list2 *SinglyLinkedNode[T]) bool {
-	for list1.Next != nil && list2.Next != nil {
+	for list1 != nil && list2 != nil {
 		if list1.Val != list2.Val {
 			return false
 		}
 		list1 = list1.Next
 		list2 = list2.Next
 	}
-	if list1.Val != list2.Val {
-		return false
-	}
-	if list1.Next != nil || list2.Next != nil {
+
+	if list1 != nil || list2 != nil {
 		return false
 	}
 	return true
+}
+
+func (node *SinglyLinkedNode[T]) String() string {
+	s, first := "[", true
+	for node != nil {
+		if first {
+			first = false
+		} else {
+			s += ","
+		}
+		s += fmt.Sprint(node.Val)
+		node = node.Next
+	}
+	return s + "]"
 }
