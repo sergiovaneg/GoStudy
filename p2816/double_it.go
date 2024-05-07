62a0deb96eb227ec74be46022c2de053a8394ad5
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-05-07 10:30:03 +0200

    Solved problem 2816 (Double It) using recursion and a boolean carry flag.

---
+++ p2816/double_it.go
@@ -0,0 +1,26 @@
+package p2816
+
+import lists "github.com/sergiovaneg/GO_leetcode/Lists"
+
+type ListNode = lists.SinglyLinkedNode[int]
+
+func doubleCarry(node *ListNode) bool {
+	node.Val <<= 1
+	if node.Next != nil && doubleCarry(node.Next) {
+		node.Val++
+	}
+
+	carry := node.Val >= 10
+	if carry {
+		node.Val -= 10
+	}
+	return carry
+}
+
+func DoubleIt(head *ListNode) *ListNode {
+	carry := doubleCarry(head)
+	if carry {
+		return &ListNode{Val: 1, Next: head}
+	}
+	return head
+}
