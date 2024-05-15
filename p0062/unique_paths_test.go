7d6a33b9fd19fcadfa61aa205b52d05910967455
Sergio Mauricio Vanegas Arias <sergio.vanegas.arias@lut.fi>
2024-05-15 16:08:57 +0300

    Added test cases to problem 62.

--- p0062/unique_paths_test.go
+++ p0062/unique_paths_test.go
@@ -1,1 +1,21 @@
 package p0062_test
+
+import (
+	"testing"
+
+	"github.com/sergiovaneg/GO_leetcode/p0062"
+)
+
+func TestUniquePaths(t *testing.T) {
+	var res int
+
+	res = p0062.UniquePaths(3, 7)
+	if res != 28 {
+		t.Fatalf("Expected 28; got %v", res)
+	}
+
+	res = p0062.UniquePaths(3, 2)
+	if res != 3 {
+		t.Fatalf("Expected 3; got %v", res)
+	}
+}
