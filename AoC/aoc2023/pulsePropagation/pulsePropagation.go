8f99af730a0da31de4701ea46493de50ac8ebe63
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-05-29 18:05:31 +0300

    Started writing the broadcast method.

--- AoC/aoc2023/pulsePropagation/pulsePropagation.go
+++ AoC/aoc2023/pulsePropagation/pulsePropagation.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"bufio"
-	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -15,28 +15,98 @@ type Node struct {
 	outputs []*Node
 }
 
+type Pulse struct {
+	src    *Node
+	target *Node
+	value  bool
+}
+
 type System map[string]*Node
 
-func (node *Node) updateClass(class byte) {
+func (node *Node) setClass(class byte) {
 	switch class {
 	case '%':
 		node.class = "flipflop"
 	case '&':
 		node.class = "conjunction"
+	case 'b':
+		node.class = "broadcaster"
+	}
+}
+
+func (node *Node) initMemory() {
+	switch node.class {
+	case "flipflop":
+		node.memory = make([]bool, 1)
+	case "conjunction":
+		node.memory = make([]bool, len(node.inputs))
 	}
 }
 
 func (system *System) registerNode(name string, class byte) *Node {
 	node := &Node{
-		memory:  make([]bool, 0),
 		inputs:  make([]*Node, 0),
 		outputs: make([]*Node, 0),
 	}
-	node.updateClass(class)
+	node.setClass(class)
 
 	(*system)[name] = node
 
 	return node
+}
+
+func emit(value bool, src *Node, targets []*Node) []Pulse {
+	pulses := make([]Pulse, len(targets))
+
+	for idx, target := range targets {
+		pulses[idx] = Pulse{
+			src:    src,
+			target: target,
+			value:  value,
+		}
+	}
+
+	return pulses
+}
+
+func (node *Node) processPulse(pulse Pulse) []Pulse {
+	switch node.class {
+	case "broadcaster":
+		return emit(pulse.value, node, node.outputs)
+	case "flipflop":
+		if pulse.value {
+			break
+		}
+		node.memory[0] = !node.memory[0]
+		return emit(node.memory[0], node, node.outputs)
+	case "conjunction":
+		idx := slices.Index(node.inputs, pulse.src)
+		node.memory[idx] = pulse.value
+
+		var value bool
+		for _, mem := range node.memory {
+			if !mem {
+				value = true
+				break
+			}
+		}
+
+		return emit(value, node, node.outputs)
+	}
+
+	return nil
+}
+
+func (system System) broadcast() {
+	var pulse Pulse
+	queue := system["broadcaster"].processPulse(pulse)
+	for len(queue) > 0 {
+		pulse, queue = queue[0], queue[1:]
+		newPulses := pulse.target.processPulse(pulse)
+		if newPulses != nil {
+			queue = append(queue, newPulses...)
+		}
+	}
 }
 
 func main() {
@@ -49,9 +119,6 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	system := make(System)
-	broadcaster := Node{
-		class: "broadcaster",
-	}
 	var src *Node
 
 	for scanner.Scan() {
@@ -63,10 +130,10 @@ func main() {
 			if src == nil {
 				src = system.registerNode(name, class)
 			} else {
-				src.updateClass(class)
+				src.setClass(class)
 			}
 		} else {
-			src = &broadcaster
+			src = system.registerNode("broadcaster", 'b')
 		}
 
 		outputs := strings.Split(srcDst[1], ", ")
@@ -82,5 +149,9 @@ func main() {
 		}
 	}
 
-	fmt.Println("ok")
+	for _, node := range system {
+		node.initMemory()
+	}
+
+	system.broadcast()
 }
