dd573b8ab4c9e8a9037d00c8812667260b734f19
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-06-06 11:33:21 +0300

    Finished solving day 24 (Never tell me the Odds) using Linear Algebra (SoE for part 2 from https://topaz.github.io/paste/#XQAAAQANDAAAAAAAAAArmUAGM0NwXMLp2PQFNgRmh8ezkFKvEQd5obzMZwooEj1R498QeYDSxeTiF400W5Ml85PHQXQ/t4JmA7LkXWLwCOuXjTUZ9cIX/jhhvcP8EwiigBA3i2aj2uwkTmPRUpEAhQcV3gFGRBZdv0QomIXupqptd/ZH+3xPKdlV6/b5otDSX93R82DTUevMK4ls0CJH7GExO62B4wsmGGLkWmUKpcUGjlRc6tnITVvD3GlONpMJeiLCP4CkT3fijH+LNeuijMoQO9curNNpIl/taPOLl36w+BZ4vroViny70gkz/pW1oV6mMkc5X+TUg+iszuafPivIAvsNmi5rdnobza5b1zLS1gG5VRBQm0xqlQ2ssvgQWD4pmrDVt0CUbQDnYN9SOUWUQhf9dH8Ih/78LE0mtSkUEX8yaTWqZkM1teONYoil2yC0gv2CcSKAAyBrWAqyiJ1IytGAMFpEnHnkWQBQHECQeQDBQwMV185CJMVXVJZvdbd673BNadRSmbY1tVaQo2Y2TAoqhglSQ2R+6vnl8a4kChB5XIMh/Rzf5Lre58IQ01Lu/EiJd7pe2ceM4U29XjhEmSUB4iQGLx3piB84WA8ixC0XLYEzjIfOKumlGMmoB2OtcBeio+MsLjBN8PdoUfiYX7EzBxv++JnFikhGKiJVDy6JWJPZgBYhYmHRVyz21u3NKwMqy4ET8shjXmT5UYh8MBQ/VHrKOF7YqDPkCvs2mm4PqRwjjMYbGmhmVXxU0emhxicM2H8XljNGsOmPk/EEziz6S3i8FDVIcFpkQkoQPLHhlI59Rg+ZePfiFn9zGbFQFfAXhiER+uKCuwndMY5ePfKhQIvdYTVVcjR5Hwhv8zMuAyAQeqHnyrce6XpPIBp5tpAvCmFTbUCDqJez5ONUX50z6gFcgtGOwM1DLOD8YAYZZdXQ03BPgcw53VdrCvdVxypRrGxPBQTHnu260g8PSSAT7oejUZC/Mx3KfPOrkThbZfylM4nijiNDi+JrReW8UeLEX3M/j7uz9r+lE6m1YnE9BYa0G6B/qdCU2QBct/lf1i5rgd2iwy63angdaCDRBNRNflV8MFBLox94kezB0lwMvussr6bSkkr52hHjcWGMwShj+URNOmkTEEEnB/Ik5HAi+BZBq8vG5LvOkWDCQRn2+x5un8XIAHjP24dzCcS/VAFtLA6CNs4nt9EtlqtS6FvEU8eI7Kx628WHFF/7l83TS4vhWMtwCqh+jr6BEna3YOoTMjkMMfibN5St+5/2dLGxUqN4vVP4p0CTqGQYH9ku+C+szbAPICIxaTjkJNZm6Ix+vJod/j0Gug==).

--- AoC/aoc2023/noOdds/noOdds.go
+++ AoC/aoc2023/noOdds/noOdds.go
@@ -1,5 +1,130 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
 
+	"github.com/sergiovaneg/GoStudy/utils"
+	"gonum.org/v1/gonum/mat"
+)
+
+const lowerBound = 200000000000000
+const upperBound = 400000000000000
+
+func parseHailstone(line string) [2][3]int {
+	matches := regexp.MustCompile("(-*[0-9]+)").FindAllString(line, 6)
+	var numbers [6]int
+
+	for idx, match := range matches {
+		numbers[idx], _ = strconv.Atoi(match)
+	}
+
+	return [2][3]int{
+		{numbers[0], numbers[1], numbers[2]},
+		{numbers[3], numbers[4], numbers[5]},
+	}
+}
+
+func countCollisionsXY(hailstones [][2][3]int) uint {
+	var res uint
+
+	for i, ha := range hailstones {
+		pa, va := ha[0], ha[1]
+		for _, hb := range hailstones[i+1:] {
+			pb, vb := hb[0], hb[1]
+
+			det := va[1]*vb[0] - va[0]*vb[1]
+			if det == 0 {
+				continue
+			}
+
+			ta := float64(vb[0]*(pb[1]-pa[1])-vb[1]*(pb[0]-pa[0])) / float64(det)
+			tb := float64(va[0]*(pb[1]-pa[1])-va[1]*(pb[0]-pa[0])) / float64(det)
+
+			if ta < 0 || tb < 0 {
+				continue
+			}
+
+			x, y := float64(pa[0])+float64(va[0])*ta, float64(pa[1])+float64(va[1])*ta
+			if x < lowerBound || x > upperBound || y < lowerBound || y > upperBound {
+				continue
+			}
+
+			res++
+		}
+	}
+
+	return res
+}
+
+func calcInitialPosition(h [][2][3]int) [3]int {
+	aData, bData := make([]float64, 0, 6*6), make([]float64, 0, 6)
+	for idx := 1; idx <= 3; idx++ {
+		aData = append(aData,
+			float64(h[0][1][1]-h[idx][1][1]),
+			float64(h[idx][1][0]-h[0][1][0]),
+			0.,
+			float64(h[idx][0][1]-h[0][0][1]),
+			float64(h[0][0][0]-h[idx][0][0]),
+			0.,
+			float64(h[0][1][2]-h[idx][1][2]),
+			0.,
+			float64(h[idx][1][0]-h[0][1][0]),
+			float64(h[idx][0][2]-h[0][0][2]),
+			0.,
+			float64(h[0][0][0]-h[idx][0][0]))
+
+		bData = append(bData,
+			float64(h[0][0][0]*h[0][1][1]-
+				h[0][0][1]*h[0][1][0]-
+				h[idx][0][0]*h[idx][1][1]+
+				h[idx][0][1]*h[idx][1][0]),
+			float64(h[0][0][0]*h[0][1][2]-
+				h[0][0][2]*h[0][1][0]-
+				h[idx][0][0]*h[idx][1][2]+
+				h[idx][0][2]*h[idx][1][0]))
+	}
+
+	a := mat.NewDense(6, 6, aData)
+	b := mat.NewVecDense(6, bData)
+
+	var x mat.VecDense
+	err := x.SolveVec(a, b)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return [3]int{
+		int(x.AtVec(0)),
+		int(x.AtVec(1)),
+		int(x.AtVec(2)),
+	}
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	n, err := utils.LineCounter(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	hailstones := make([][2][3]int, n)
+	for idx := 0; scanner.Scan(); idx++ {
+		hailstones[idx] = parseHailstone(scanner.Text())
+	}
+
+	println(countCollisionsXY(hailstones))
+
+	initialPosition := calcInitialPosition(hailstones)
+	println(initialPosition[0] + initialPosition[1] + initialPosition[2])
 }
