09e15daefaf403f43f2340ffcc0dbd8435eb24f6
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-06-24 10:08:52 +0300

    Solved part 1 of day 19; part 2 left for later.

--- AoC/aoc2015/medicineRudolph/medicineRudolph.go
+++ AoC/aoc2015/medicineRudolph/medicineRudolph.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"log"
 	"os"
+	"regexp"
 	"strings"
 )
 
@@ -11,11 +12,43 @@ type Plant map[string][]string
 
 func (p *Plant) addTransformation(line string) {
 	molecules := strings.Split(line, " => ")
-	if _, exists := (*p)[molecules[0]]; exists {
-		(*p)[molecules[0]] = append((*p)[molecules[0]], molecules[1])
-	} else {
-		(*p)[molecules[0]] = []string{molecules[1]}
+	(*p)[molecules[0]] = append((*p)[molecules[0]], molecules[1])
+}
+
+func (p Plant) mapSingleStep(initialMolecule string) map[string]bool {
+	record := make(map[string]bool, len(initialMolecule))
+	for key, candidates := range p {
+		for _, loc := range regexp.MustCompile(
+			"("+key+")").FindAllStringIndex(initialMolecule, -1) {
+			for _, rep := range candidates {
+				newMolecule := initialMolecule[:loc[0]] + rep + initialMolecule[loc[1]:]
+				record[newMolecule] = true
+			}
+		}
 	}
+	return record
+}
+
+func (p Plant) getMinSteps(target string) int {
+
+	stepsMap := map[string]int{
+		"e": 0,
+	}
+	for steps := 0; stepsMap[target] == 0; steps++ {
+		for initialMolecule, recordedSteps := range stepsMap {
+			if recordedSteps < steps {
+				continue
+			}
+			nextMolecules := p.mapSingleStep(initialMolecule)
+			for next := range nextMolecules {
+				if _, ok := stepsMap[next]; !ok {
+					stepsMap[next] = steps + 1
+				}
+			}
+		}
+	}
+
+	return stepsMap[target]
 }
 
 func main() {
@@ -28,9 +61,13 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	p := make(Plant)
 	for scanner.Scan() {
-		if scanner.Text() == "\n" {
+		if scanner.Text() == "" {
 			break
 		}
 		p.addTransformation(scanner.Text())
 	}
+
+	scanner.Scan()
+	println(len(p.mapSingleStep(scanner.Text())))
+	println(p.getMinSteps(scanner.Text()))
 }
