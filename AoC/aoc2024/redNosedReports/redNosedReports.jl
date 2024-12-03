ad9c998f5fcaf2e3a42d489cb98b9e2c72a81d26
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-12-03 21:52:09 +0100

    Reimplemented day 2, 2024 in Julia

---
+++ AoC/aoc2024/redNosedReports/redNosedReports.jl
@@ -0,0 +1,49 @@
+levels = readlines("./input.txt")
+
+function parseLevel(level::String)::Vector{Int}
+  res = map(
+    num -> parse(Int, num, base=10),
+    split(level, " ")
+  )
+  return res
+end
+
+parsed_levels = map(
+  parseLevel,
+  levels
+)
+
+function isSafeLevel(level::Vector{Int}, tol::Int)::Bool
+  deltas = diff(level)
+  idx = findfirst(
+    d -> d == 0 || abs(d) > 3 || sign(d) != sign(deltas[1]),
+    deltas
+  )
+  if isnothing(idx)
+    return true
+  elseif tol == 0
+    return false
+  else
+    return isSafeLevel(
+      vcat(level[1:idx-1], level[idx+1:end]),
+      tol - 1
+    ) || isSafeLevel(
+      vcat(level[1:idx], level[idx+2:end]),
+      tol - 1
+    )
+  end
+end
+
+function isSafeLevel(level::Vector{Int})::Bool
+  return isSafeLevel(level, 0)
+end
+
+map(
+  isSafeLevel,
+  parsed_levels
+) |> count |> println
+
+map(
+  l -> isSafeLevel(l, 1),
+  parsed_levels
+) |> count |> println
