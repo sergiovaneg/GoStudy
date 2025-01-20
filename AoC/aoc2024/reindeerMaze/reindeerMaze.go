b1b1de02be3cdc12bd57285bf23d0acc027ca0f4
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2025-01-20 14:21:34 +0100

    Minor refactor

--- AoC/aoc2024/reindeerMaze/reindeerMaze.go
+++ AoC/aoc2024/reindeerMaze/reindeerMaze.go
@@ -21,7 +21,7 @@ type Traversed map[Coordinate]bool
 
 type ParallelRecord struct {
 	best int
-	mu   sync.Mutex
+	mu   *sync.Mutex
 	rec  Record
 	m    Maze
 	t    Traversed
@@ -43,13 +43,13 @@ func initWalker(acc int, s State, t Traversed) Walker {
 	return Walker{acc: acc, s: s, t: newT}
 }
 
-func (t *Traversed) merge(tOther Traversed) {
+func (tSelf *Traversed) merge(tOther Traversed) {
 	for k := range tOther {
-		(*t)[k] = true
+		(*tSelf)[k] = true
 	}
 }
 
-func (pr *ParallelRecord) getCandidateStates(s State) []State {
+func (pr ParallelRecord) getCandidateStates(s State) []State {
 	candidates := make([]State, 0)
 	x0 := s[0]
 	for _, dx := range [4]Coordinate{
@@ -67,23 +67,26 @@ func (pr *ParallelRecord) getCandidateStates(s State) []State {
 
 func getTransitionCost(s0, s1 State) int {
 	v0, v1 := s0[1], s1[1]
-	switch v0[0]*v1[0] + v0[1]*v1[1] {
-	case 1:
+	if dot := v0[0]*v1[0] + v0[1]*v1[1]; dot > 0 {
 		return stepCost
-	case 0:
-		return rotCost + stepCost
-	case -1:
+	} else if dot < 0 {
 		return rotCost<<1 + stepCost
+	} else {
+		return rotCost + stepCost
 	}
-
-	return -1
 }
 
-func (pr *ParallelRecord) parallelBFS(w Walker) {
+func (pr *ParallelRecord) parallelBFS(w Walker, end Coordinate) {
 	var wg sync.WaitGroup
 
 	for {
+		// Atomic part
 		pr.mu.Lock()
+
+		if pr.best != -1 && w.acc > pr.best {
+			pr.mu.Unlock()
+			break
+		}
 
 		currentBest, exists := pr.rec[w.s]
 		if !exists || w.acc <= currentBest {
@@ -93,7 +96,7 @@ func (pr *ParallelRecord) parallelBFS(w Walker) {
 			break
 		}
 
-		if x := w.s[0]; pr.m[x[0]][x[1]] == 'E' {
+		if x := w.s[0]; x == end {
 			if pr.best == -1 || w.acc <= pr.best {
 				if pr.best == -1 || w.acc < pr.best {
 					pr.best = w.acc
@@ -101,6 +104,7 @@ func (pr *ParallelRecord) parallelBFS(w Walker) {
 				}
 				pr.t.merge(w.t)
 			}
+
 			pr.mu.Unlock()
 			break
 		}
@@ -110,14 +114,16 @@ func (pr *ParallelRecord) parallelBFS(w Walker) {
 		candidateStates := pr.getCandidateStates(w.s)
 		wg.Add(len(candidateStates) - 1)
 		for _, s := range candidateStates[1:] {
-			go func(newW Walker) {
-				pr.parallelBFS(newW)
-				wg.Done()
-			}(initWalker(
+			wNew := initWalker(
 				w.acc+getTransitionCost(w.s, s),
 				s,
 				w.t,
-			))
+			)
+
+			go func() {
+				pr.parallelBFS(wNew, end)
+				wg.Done()
+			}()
 		}
 
 		w.acc += getTransitionCost(w.s, candidateStates[0])
@@ -144,23 +150,7 @@ func (m Maze) getEndpoints() (start, end Coordinate) {
 	return
 }
 
-func (pr *ParallelRecord) printFinalState() {
-	pr.mu.Lock()
-	m := make(Maze, len(pr.m))
-	for i, row := range pr.m {
-		m[i] = make([]byte, len(row))
-		copy(m[i], row)
-	}
-	for k := range pr.t {
-		m[k[0]][k[1]] = 'O'
-	}
-	for _, row := range m {
-		println(string(row))
-	}
-	pr.mu.Unlock()
-}
-
-func main() {
+func initParallelRecord() ParallelRecord {
 	file, err := os.Open("./input.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -170,23 +160,31 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	n, _ := utils.LineCounter(file)
 
-	var pr ParallelRecord
-	pr.rec = make(Record)
-	pr.m = make(Maze, n)
-	pr.best = -1
+	pr := ParallelRecord{
+		best: -1,
+		mu:   new(sync.Mutex),
+		rec:  make(Record),
+		m:    make(Maze, n),
+		t:    nil,
+	}
 
 	for i := 0; scanner.Scan(); i++ {
 		pr.m[i] = []byte(scanner.Text())
 	}
 
-	start, _ := pr.m.getEndpoints()
+	return pr
+}
+
+func main() {
+	pr := initParallelRecord()
+
+	start, end := pr.m.getEndpoints()
 	pr.parallelBFS(initWalker(
 		0,
 		State{start, Coordinate{0, 1}},
 		nil,
-	))
+	), end)
 
 	println(pr.best)
-	pr.printFinalState()
 	println(len(pr.t))
 }
