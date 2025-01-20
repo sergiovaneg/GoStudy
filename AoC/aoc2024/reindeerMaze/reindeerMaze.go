65940fb1693110735b2f3644bc8aa79581811635
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2025-01-20 11:53:18 +0100

    Unoptimized solution for day 16, 2024 (Reindeer Maze); todo: improve data structure.

--- AoC/aoc2024/reindeerMaze/reindeerMaze.go
+++ AoC/aoc2024/reindeerMaze/reindeerMaze.go
@@ -14,28 +14,52 @@ const stepCost = 1
 
 type Coordinate [2]int
 type State [2]Coordinate
-type Record map[State]int
 type Maze [][]byte
 
+type Record map[State]int
+type Traversed map[Coordinate]bool
+
 type ParallelRecord struct {
-	mu  sync.Mutex
-	rec Record
-	m   Maze
+	best int
+	mu   sync.Mutex
+	rec  Record
+	m    Maze
+	t    Traversed
 }
 
-func (pr *ParallelRecord) getCandidates(s State) []State {
+type Walker struct {
+	acc int
+	s   State
+	t   Traversed
+}
+
+func initWalker(acc int, s State, t Traversed) Walker {
+	newT := make(Traversed)
+	for k := range t {
+		newT[k] = true
+	}
+	newT[s[0]] = true
+
+	return Walker{acc: acc, s: s, t: newT}
+}
+
+func (t *Traversed) merge(tOther Traversed) {
+	for k := range tOther {
+		(*t)[k] = true
+	}
+}
+
+func (pr *ParallelRecord) getCandidateStates(s State) []State {
 	candidates := make([]State, 0)
 	x0 := s[0]
 	for _, dx := range [4]Coordinate{
-		{0, -1},
-		{0, 1},
-		{-1, 0},
-		{1, 0},
+		{0, -1}, {0, 1}, {-1, 0}, {1, 0},
 	} {
 		x1 := Coordinate{x0[0] + dx[0], x0[1] + dx[1]}
-		if pr.m[x1[0]][x1[1]] != '#' {
-			candidates = append(candidates, State{x1, dx})
+		if pr.m[x1[0]][x1[1]] == '#' {
+			continue
 		}
+		candidates = append(candidates, State{x1, dx})
 	}
 
 	return candidates
@@ -48,67 +72,92 @@ func getTransitionCost(s0, s1 State) int {
 		return stepCost
 	case 0:
 		return rotCost + stepCost
-	default:
+	case -1:
 		return rotCost<<1 + stepCost
 	}
+
+	return -1
 }
 
-func (pr *ParallelRecord) parallelDFS(s0 State, accCost int) {
+func (pr *ParallelRecord) parallelBFS(w Walker) {
 	var wg sync.WaitGroup
 
 	for {
 		pr.mu.Lock()
-		currentBest, exists := pr.rec[s0]
-		if !exists || accCost < currentBest {
-			pr.rec[s0] = accCost
-			pr.mu.Unlock()
+
+		currentBest, exists := pr.rec[w.s]
+		if !exists || w.acc <= currentBest {
+			pr.rec[w.s] = w.acc
 		} else {
 			pr.mu.Unlock()
 			break
 		}
 
-		candidates := pr.getCandidates(s0)
-		for _, s1 := range candidates[1:] {
-			wg.Add(1)
-			go func(newS State, newCost int) {
-				pr.parallelDFS(newS, newCost)
-				wg.Done()
-			}(s1, accCost+getTransitionCost(s0, s1))
+		if x := w.s[0]; pr.m[x[0]][x[1]] == 'E' {
+			if pr.best == -1 || w.acc <= pr.best {
+				if pr.best == -1 || w.acc < pr.best {
+					pr.best = w.acc
+					pr.t = make(Traversed)
+				}
+				pr.t.merge(w.t)
+			}
+			pr.mu.Unlock()
+			break
 		}
 
-		accCost += getTransitionCost(s0, candidates[0])
-		s0 = candidates[0]
+		pr.mu.Unlock()
+
+		candidateStates := pr.getCandidateStates(w.s)
+		wg.Add(len(candidateStates) - 1)
+		for _, s := range candidateStates[1:] {
+			go func(newW Walker) {
+				pr.parallelBFS(newW)
+				wg.Done()
+			}(initWalker(
+				w.acc+getTransitionCost(w.s, s),
+				s,
+				w.t,
+			))
+		}
+
+		w.acc += getTransitionCost(w.s, candidateStates[0])
+		w.s = candidateStates[0]
+		w.t[w.s[0]] = true
 	}
 
 	wg.Wait()
 }
 
-func (m Maze) getLandmarks() (start, end Coordinate) {
+func (m Maze) getEndpoints() (start, end Coordinate) {
 	for i, row := range m {
 		for j, val := range row {
-			if val == 'S' {
+			switch val {
+			case 'S':
 				start = Coordinate{i, j}
-			} else if val == 'E' {
+			case 'E':
 				end = Coordinate{i, j}
+			default:
+				continue
 			}
 		}
 	}
 	return
 }
 
-func (rec Record) getMinScore(end Coordinate) int {
-	minVal := -1
-
-	for k, v := range rec {
-		if k[0] != end {
-			continue
-		}
-		if minVal == -1 || v < minVal {
-			minVal = v
-		}
+func (pr *ParallelRecord) printFinalState() {
+	pr.mu.Lock()
+	m := make(Maze, len(pr.m))
+	for i, row := range pr.m {
+		m[i] = make([]byte, len(row))
+		copy(m[i], row)
 	}
-
-	return minVal
+	for k := range pr.t {
+		m[k[0]][k[1]] = 'O'
+	}
+	for _, row := range m {
+		println(string(row))
+	}
+	pr.mu.Unlock()
 }
 
 func main() {
@@ -124,12 +173,20 @@ func main() {
 	var pr ParallelRecord
 	pr.rec = make(Record)
 	pr.m = make(Maze, n)
+	pr.best = -1
 
 	for i := 0; scanner.Scan(); i++ {
 		pr.m[i] = []byte(scanner.Text())
 	}
 
-	s, e := pr.m.getLandmarks()
-	pr.parallelDFS(State{s, Coordinate{0, -1}}, 0)
-	println(pr.rec.getMinScore(e))
+	start, _ := pr.m.getEndpoints()
+	pr.parallelBFS(initWalker(
+		0,
+		State{start, Coordinate{0, 1}},
+		nil,
+	))
+
+	println(pr.best)
+	pr.printFinalState()
+	println(len(pr.t))
 }
