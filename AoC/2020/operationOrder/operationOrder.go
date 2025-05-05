75e165cc4991a721e1a7c3c57ee556d10cd981a8
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2025-05-05 17:06:32 +0200

    Solved day 18, 2020 (Operation Order) using recursion and higher-order functions.

---
+++ AoC/2020/operationOrder/operationOrder.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	"github.com/sergiovaneg/GoStudy/utils"
+)
+
+func operateBasic(expr string) int {
+	numbers := regexp.MustCompile(`\d+`).FindAllString(expr, -1)
+	ops := regexp.MustCompile(`[\+|\*]{1}`).FindAllString(expr, -1)
+
+	res, _ := strconv.Atoi(numbers[0])
+	for idx, op := range ops {
+		aux, _ := strconv.Atoi(numbers[idx+1])
+		if op == "*" {
+			res *= aux
+		} else {
+			res += aux
+		}
+	}
+
+	return res
+}
+
+func operateAdvanced(expr string) int {
+	res := 1
+
+	for factor := range strings.SplitSeq(expr, " * ") {
+		var acc int
+		for _, num := range regexp.MustCompile(`\d+`).FindAllString(factor, -1) {
+			val, _ := strconv.Atoi(num)
+			acc += val
+		}
+		res *= acc
+	}
+
+	return res
+}
+
+func eval(expr string, reducer func(string) int) int {
+	groups := make([]string, 0)
+
+	level, lb := 0, -1
+	for idx, r := range expr {
+		if r == '(' {
+			if level == 0 {
+				lb = idx
+			}
+			level++
+		} else if r == ')' {
+			level--
+			if level == 0 {
+				groups = append(groups, expr[lb:idx+1])
+			}
+		}
+	}
+
+	for _, group := range groups {
+		n := len(group)
+		expr = strings.ReplaceAll(
+			expr, group, strconv.Itoa(eval(group[1:n-1], reducer)))
+	}
+
+	return reducer(expr)
+}
+
+func main() {
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	n, _ := utils.LineCounter(file)
+
+	cA, cB := make(chan int, n), make(chan int, n)
+
+	for scanner.Scan() {
+		go func(line string) {
+			cA <- eval(line, operateBasic)
+			cB <- eval(line, operateAdvanced)
+		}(scanner.Text())
+	}
+
+	var resA, resB int
+	for range n {
+		resA += <-cA
+		resB += <-cB
+	}
+	println(resA)
+	println(resB)
+}
